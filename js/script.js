4441f94a24e4910920d86d6878976e1bd0272561
Ryan <r.alcoran@gmail.com>
2023-03-06 11:44:09 -0500

    Added hover effect to change background color of the grid's divs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
+const defaultSize = 16;
 const grid = document.querySelector('.grid');
 
-for (var i = 0; i < 16; i++) {
+for (var i = 0; i < defaultSize; i++) {
 	const row = document.createElement('div');
-    row.classList.add('row');
+	row.classList.add('row');
     grid.appendChild(row);
-	for (j = 0; j < 16; j++) {
-		const cell = document.createElement('div');
-        cell.classList.add('cell');
+	for (j = 0; j < defaultSize; j++) {
+        const cell = document.createElement('div');
+		cell.classList.add('cell');
+		cell.addEventListener("mouseover", (event) => {
+	event.target.style.backgroundColor = "darkcyan";
+});
 	    row.appendChild(cell);
 	}
 }
