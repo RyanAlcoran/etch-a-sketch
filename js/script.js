28fbf6fb96b138d0b9727ad16bc040c30a0fe3ed
Ryan <r.alcoran@gmail.com>
2023-03-07 11:38:04 -0500

    Added functionality to create grid based on size and color. Added html buttons to Resize and Reset.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 const defaultSize = 16;
+const defaultContainerSize = 750;
 const grid = document.querySelector('.grid');
+const defaultColor = "darkcyan"
 
-for (var i = 0; i < defaultSize; i++) {
+function createGrid(size, color){
+	let cellSize = getCellSize(size);
+	for (var i = 0; i < size; i++) {
 	const row = document.createElement('div');
+	//row.style.width = defaultContainerSize.toString() + "px";
 	row.classList.add('row');
     grid.appendChild(row);
-	for (j = 0; j < defaultSize; j++) {
-        const cell = document.createElement('div');
-		cell.classList.add('cell');
-		cell.addEventListener("mouseover", (event) => {
-	event.target.style.backgroundColor = "darkcyan";
-});
-	    row.appendChild(cell);
-	}
+		for (j = 0; j < size; j++) {
+	        const cell = document.createElement('div');
+			cell.classList.add('cell');
+			cell.style.width = cellSize.toString() + "px";
+			cell.style.height = cellSize.toString() + "px";
+			cell.addEventListener("mouseover", (event) => {
+				event.target.style.backgroundColor = color;
+			});
+		    row.appendChild(cell);
+		};
+	};	
 }
+
+function getCellSize(size){
+	return defaultContainerSize / size;
+}
+createGrid(defaultSize, defaultColor);
